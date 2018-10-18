4f8107bee5c68b773eba75ea80eb415463ec1a03
moumoumoumouna <moumoumoumouna@gmail.com>
2018-10-18 16:34:12 +0900

    first commit

--- app/src/main/java/com/example/moumouna/gitbranchtest/firstbranch/firstbranch2.java
+++ app/src/main/java/com/example/moumouna/gitbranchtest/firstbranch/firstbranch2.java
@@ -4,7 +4,7 @@ package com.example.moumouna.gitbranchtest.firstbranch;
  * Created by moumouna on 2018-10-18.
  */
 
-public class firshbranch2 {
-    int firstbranch2 = 1;
+public class firstbranch2 {
+    int firstbranch2 = 2;
 
 }
