794e65548829c6fd1fe9ef3477608058a77f97f1
moumoumoumouna <moumoumoumouna@gmail.com>
2018-10-18 16:26:59 +0900

    first commit

---
+++ app/src/main/java/com/example/moumouna/gitbranchtest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.moumouna.gitbranchtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
